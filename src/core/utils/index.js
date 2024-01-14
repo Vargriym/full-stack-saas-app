aa010a1d84f9085ab917892e0a67b844851f57d4
Moe Boraidini <102037554+Vargriym@users.noreply.github.com>
2024-01-14 16:23:19 +0200

    Update index.js

--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -1,3 +1,3 @@
 const production = process.env.NODE_ENV === "production"
 
-export const SITE_URL = production ? "prod-url" : "https://full-stack-saas-app.vercel.app" 
+export const SITE_URL = production ? "https://full-stack-saas-app.vercel.app" : "http://localhost:3000" 
