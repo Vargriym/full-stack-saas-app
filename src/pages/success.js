6a1f86e6f2a88bfc99ac04e062f6660e2bc8c18d
Moe Boraidini <102037554+Vargriym@users.noreply.github.com>
2024-01-14 15:58:01 +0200

    Update success.js

--- src/pages/success.js
+++ src/pages/success.js
@@ -14,7 +14,7 @@ export default function SuccessPage() {
           alt="confetti"
           className="confetti"
           />
-          <h1>You're in!</h1>
+          <h1>{`You're in!`}</h1>
           <p>You can now access everything on the site. <br /> Ready to get started?</p>
           <Link href="/login" className="large-button">
             <div className="large-button-text">Go to login</div>
